a4f6ff5fc2b2263c55bb97a5e1b6dc94ae3609e3
GitUserRSA <104914459+GitUserRSA@users.noreply.github.com>
2024-08-30 00:29:31 +0200

    Cart actions
    Created interface and implementation of cart actions, a class that handles backend processing for the shopping cart.
    
    Added new dbsets to make the class functional.
    
    Added dependency injection for cart actions.

---
+++ SAOnlineMart/Services/Interface/ICartActions.cs
@@ -0,0 +1,21 @@
+﻿using SAOnlineMart.Models;
+
+namespace SAOnlineMart.Services.Interface
+{
+    public partial interface ICartActions
+    {
+        public void AddToCart(ProductsModel products);
+
+        public string GetCartId();
+
+        public List<CartModel> GetCartItems();
+
+        public int GetTotal();
+
+        public void EmptyCart();
+
+        public int RemoveFromCart(int id);
+
+        public int CreateOrder(OrderModel order);
+    }
+}
